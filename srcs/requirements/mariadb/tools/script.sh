aa8eb4a94c070ca0e39eca993066c4a45a8f3606
albartol <albartol@student.42madrid.com>
2025-07-28 21:37:48 +0200

    First working ver

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+envsubst '$DB_NAME,$MDB_USER,$MDB_USER_PASS,$MDB_ROOT_USER,$MDB_ROOT_PASS' \
+    < database-env > database.sql
+
 mariadbd --user=mysql --init-file=/server/database.sql
--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -4,4 +4,6 @@ openssl req -x509 -nodes -days 42 -newkey rsa:4096 \
 -keyout /server/ssl/${LOGIN}.key -out /server/ssl/${LOGIN}.crt \
 -subj "/C=ES/ST=Madrid/L=Madrid/O=42Madrid/CN=${DOMAIN}"
 
+envsubst '$DOMAIN,$LOGIN' < nginx.conf > /etc/nginx/http.d/inception.conf
+
 nginx -g 'daemon off;'
